46925c2771a32eb00ba16ba4ec547cc7ef136577
ll__ <loatheb.zhao@gmail.com>
2022-04-02 10:04:31 +0800

    OK-7232: add new analytic script (#14)

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,19 +1,16 @@
 import Head from "next/head";
 import React from "react";
 
-const content = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'UA-187315652-1');`;
-
 const Header: React.FC = function (prop) {
   return (
     <Head>
       {process.env.NODE_ENV === "production" ? (
-        <>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-187315652-1"
-          />
-          <script dangerouslySetInnerHTML={{ __html: content }} />
-        </>
+        <script
+          src="https://jarvis-website.onekey.so/script.js"
+          data-site="WWLGDZJB"
+          data-spa="auto"
+          defer
+        />
       ) : null}
       {prop.children}
     </Head>
